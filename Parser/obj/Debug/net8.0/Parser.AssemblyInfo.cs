df8945308b86fd604dc7a57bd01a567ac400812e
VigilantLizard <nat20fen@gmail.com>
2024-10-30 04:06:11 +0000

    e

--- Parser/obj/Debug/net8.0/Parser.AssemblyInfo.cs
+++ Parser/obj/Debug/net8.0/Parser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cedc43ea5a682797b2474f58a45a9c2a64ae21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cba48213ab14d124c9a7a4d148cfb4caabb50b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
